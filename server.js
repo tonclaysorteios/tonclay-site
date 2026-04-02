76301dbecc9986e0ae2992efac1ba833597be46d
Clayton Santos <tonclaysorteios@gmail.com>
2026-04-02 05:19:20 -0300

    ajuste render port

--- server.js
+++ server.js
@@ -60,6 +60,8 @@ app.get('/', (req, res) => {
 });
 
 // 🚀 START
-app.listen(3000, () => {
-    console.log('🚀 Servidor rodando em http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
